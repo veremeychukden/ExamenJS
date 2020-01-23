4531d2c590effa5e30aa5ef829bca4902f75db9c
Денис Веремейчук <Veremeychuk@rivne.itstep.org>
2020-01-23 19:49:44 +0200

    Finished

--- main.js
+++ main.js
@@ -1,66 +1,101 @@
 var url = "https://poloniex.com/public?command=returnCurrencies";
+var result;
 
-class Currency {
-    name;
-    humanType;
-    currencyType;
-    txFee;
-    minConf;
-    constructor(_name, _humanType, _currencyType, _txFee, _minConf) {
-        this.name = _name;
-        this.humanType = _humanType;
-        this.currencyType = _currencyType;
-        this.txFee = _txFee;
-        this.minConf = _minConf;
-    }
-}
+async function init() {
+    //  var promise = fetch(url);
 
-var result = [];
-async function showCurrency() {
+    //   promise.then(data => {
+    //     result = Object.keys(data).map(function (key) {
+    //         return data[key];
+    //     });
+    // })
+    // .catch(error => {
+
+    //         console.log("Error! " + error);
+
+    // });
+
     let response = await fetch(url);
+
     let data = await response.json();
-    dataNext = data.next;
-    dataPrev = data.previous;
     result = Object.keys(data).map(function (key) {
         return data[key];
     });
-    for (var i = 0; i < 15; i++) {
-        result.push(document.getElementById("myBody").innerHTML +=
-            `<tr><td>${result[i].id}</td> 
-        <td>${result[i].name}</td>
-         <td>${result[i].humanType}</td> 
-         <td>${result[i].currencyType}</td> 
-         <td>${result[i].txFee}</td> 
-         <td>${result[i].minConf}</td> 
-         <td><button onclick='delCurrency(${i})' style='color:black;'>Delete</button></td>
-         `);
-    }
-}
 
 showCurrency();
+}
 
-async function SearchF() {
-    let response = await fetch(url);
-    let data = await response.json();
-    result = Object.keys(data).map(function (key) {
-        return data[key];
-    });
+
+
+function showCurrency() {
+
+    $("#loaderId").show();
     $("#myBody").html("");
-    for (var i = 0; i < 15; i++) {
+    dry();
+
+    for (var i = 0; i < result.length; i++) {
+        document.getElementById("myBody").innerHTML +=
+            `
+            <tr><td>${result[i].id}</td> 
+                    <td>${result[i].name}</td>
+                    <td>${result[i].humanType}</td> 
+                    <td>${result[i].currencyType}</td> 
+                    <td>${result[i].txFee}</td> 
+                    <td>${result[i].minConf}</td> 
+                    <td><button onclick='DeleteF(${i})' type="button" class="btn btn-dark">Delete</button></td>
+                    `;
+    }
+    $("#loaderId").hide();
+
+}init();
+
+
+
+
+
+function SearchF() {
+    $("#myBody").html("");
+    dry();
+    
+    for (var i = 0; i < result.length; i++) {
         if (result[i].name.toLowerCase().includes($("#search").val().toLowerCase())) {
             var tmp = $("#search").val();
             document.getElementById("myBody").innerHTML +=
                 `
             <tr><td>${result[i].id}</td> 
-            <td>${result[i].name.replace(eval("/" + tmp + "/gi"), "<a name=" + tmp + " style='background:yellow'>" + tmp + "</a>")}</td>
+            <td>${result[i].name.replace(eval("/" + tmp + "/gi"), "<a name=" + tmp + " style='background:yellow;'>" + tmp + "</a>")}</td>
             <td>${result[i].humanType}</td> 
             <td>${result[i].currencyType}</td> 
             <td>${result[i].txFee}</td> 
             <td>${result[i].minConf}</td> 
-            <td><button onclick='delCurrency(${i})' style='color:black;'>Delete</button></td></tr>
+            <td><button onclick='DeleteF(${i})' style='color:black;'>Delete</button></td></tr>
                 `;
         }
     }
+    $("#loaderId").hide();
 
 }
+
+function DeleteF(index) {
+    result.splice(index, 1);
+    showCurrency();
+}
+
+
+function dry(){
+    document.getElementById("myBody").innerHTML +=
+    `<thead>
+    <tr>
+      <th>Id</th>
+      <th>Name</th>
+      <th>humanType</th scope="col">
+      <th>currencyType</th>
+      <th>TxFee</th>
+      <th>minConf</th>
+      <th>Action</th>
+    </tr>
+  </thead>
+    `;
+}
+
 
